09f922018be4fb3204af9fe10b893e94d8da357d
Keldorn17 <patai17.zoltan17@gmail.com>
2025-05-16 12:32:36 +0200

    5/16/2025

--- Section_5/src/CodingExercise.java
+++ Section_5/src/CodingExercise.java
@@ -1,5 +1,7 @@
 public class CodingExercise {
 
+    private static final double INVALID_VALUE = -1.0;
+
     public static void main(String[] args) {
 
     }
@@ -45,6 +47,69 @@ public class CodingExercise {
             return false;
         }
         return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    }
+
+    public static boolean areEqualByThreeDecimalPlaces(double number1, double number2) {
+        return (int) (number1 * 1000) == (int) (number2 * 1000);
+    }
+
+    public static boolean hasEqualSum(int first, int second, int third) {
+        return (first + second) == third;
+    }
+
+    public static boolean hasTeen(int first, int second, int third) {
+        return isTeen(first) || isTeen(second) || isTeen(third);
+    }
+
+    public static boolean isTeen(int age) {
+        return age >= 13 && age <= 19;
+    }
+
+    public static double area(double radius) {
+        double result = INVALID_VALUE;
+        if (radius >= 0) {
+            result = Math.PI * Math.pow(radius, 2);
+        }
+        return result;
+    }
+
+    public static double area(double x, double y) {
+        double result = INVALID_VALUE;
+        if (x >= 0 && y >= 0) {
+            result = x * y;
+        }
+        return result;
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        int days = (int) (minutes / 60 / 24);
+        int years = days / 365;
+        int daysRemaining = days % 365;
+        System.out.printf(minutes + " min = " + years + " y and " + daysRemaining + " d");
+    }
+
+    public static void printEqual(int a, int b, int c) {
+        if (a < 0 || b < 0 || c < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        if (a == b && a == c) {
+            System.out.println("All numbers are equal");
+        } else if (a != b && a != c && b != c) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        final int minTemp = 25;
+        final int maxTemp = summer ? 45 : 35;
+        return temperature >= minTemp && temperature <= maxTemp;
     }
 
 }
--- Section_5/src/Main.java
+++ Section_5/src/Main.java
@@ -12,6 +12,16 @@ public class Main {
         displayHighScorePosition("Test3", calculateHighScorePosition(500));
         displayHighScorePosition("Test4", calculateHighScorePosition(100));
         displayHighScorePosition("Test5", calculateHighScorePosition(25));
+
+        int newScore = calculateScore("Tim", 500);
+        System.out.println("New score is " + newScore);
+        System.out.println("New score is " + calculateScore(75));
+
+        calculateScore();
+        System.out.println(convertToCentimeters(5, 6));
+
+        System.out.println(getDurationString(3945));
+        System.out.println(getDurationString(65, 45));
     }
 
     private static int calculateScore(int score, int levelCompleted, int bonus) {
@@ -35,4 +45,50 @@ public class Main {
     private static void displayHighScorePosition(String playerName, int highScorePosition) {
         System.out.println(playerName + " managed to get into position " + highScorePosition + " on the high score list");
     }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+        return calculateScore("Anonymous", score);
+    }
+
+    public static void calculateScore() {
+        System.out.println("No player name, no player score.");
+    }
+
+    public static double convertToCentimeters(int inches) {
+        return Math.round(inches * 2.54 * 100) / 100.0;
+    }
+
+    public static double convertToCentimeters(int foot, int inches) {
+        return convertToCentimeters(foot * 12 + inches);
+    }
+
+    public static String getDurationString(int seconds) {
+        if (seconds < 0) {
+            throw new IllegalArgumentException("Seconds cannot be negative!");
+        }
+
+        int minutes = seconds / 60;
+        int secondsRemaining = seconds % 60;
+        return getDurationString(minutes, secondsRemaining);
+    }
+
+    public static String getDurationString(int minutes, int seconds) {
+        if (minutes < 0) {
+            throw new IllegalArgumentException("Minutes cannot be negative!");
+        }
+        if (seconds < 0 || seconds > 59) {
+            throw new IllegalArgumentException("Seconds has to be between 0 and 59!");
+        }
+
+        int hours = minutes / 60;
+        int minutesRemaining = minutes % 60;
+        return String.format("%02dh %02dm %02ds", hours, minutesRemaining, seconds);
+    }
+
+
 }
