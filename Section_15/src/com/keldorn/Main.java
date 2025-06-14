19129fac34f9aa978a67665860ac821bc5d0ba3d
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-06-14 19:41:57 +0200

    6/14/2025

--- Section_15/src/com/keldorn/Main.java
+++ Section_15/src/com/keldorn/Main.java
@@ -1,10 +1,10 @@
 package com.keldorn;
 
+import com.keldorn.model.card.Card;
 import com.keldorn.model.card.Deck;
+import com.keldorn.model.card.Suit;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -35,7 +35,115 @@ public class Main {
     }
 
     private static void cardTest() {
+        separator();
         Deck deck = new Deck();
         deck.printDeck();
+
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Suit.HEART, "A");
+        Arrays.fill(cardArray, aceOfHearts);
+        Deck.printDeck(Arrays.asList(cardArray), "Aces of Hearts");
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);
+        System.out.println(cards);
+        System.out.println("cards.size() = " + cards.size());
+
+        List<Card> acesOfHearts = Collections.nCopies(13, aceOfHearts);
+        Deck.printDeck(acesOfHearts, "Aces of Hearts");
+
+        Card kingOfClubs = Card.getFaceCard(Suit.CLUB, "K");
+        List<Card> kingsOfClubs = Collections.nCopies(13, kingOfClubs);
+        Deck.printDeck(kingsOfClubs, "Kings of Clubs");
+
+        Collections.addAll(cards, cardArray);
+        Collections.addAll(cards, cardArray);
+        Deck.printDeck(cards, "Card Collection with Aces added");
+
+        Collections.copy(cards, kingsOfClubs);
+        Deck.printDeck(cards, "Card Collection with Kings copied");
+
+        cards = List.copyOf(kingsOfClubs);
+        Deck.printDeck(cards, "List Copy of Kings");
+
+        Deck.printDeck(deck.getShuffledDeck(), "Shuffled Deck");
+
+        List<Card> fullDeck = deck.getStandardDeck();
+        Collections.reverse(fullDeck);
+        Deck.printDeck(fullDeck, "Reversed Deck of Cards");
+
+        var sortingAlgorithm = Comparator.comparing(Card::rank)
+                .thenComparing(Card::suit);
+        Collections.sort(fullDeck, sortingAlgorithm);
+        Deck.printDeck(fullDeck, "Standard Deck sorted by rank, suit");
+
+        Collections.reverse(fullDeck);
+        List<Card> kings = new ArrayList<>(fullDeck.subList(4, 8));
+        Deck.printDeck(kings, "Kings in deck");
+
+        List<Card> tens = new ArrayList<>(fullDeck.subList(16, 20));
+        Deck.printDeck(tens, "Tens in deck");
+
+//        Collections.shuffle(fullDeck);
+        int subListIndex = Collections.indexOfSubList(fullDeck, tens);
+        System.out.println("sublist index for tens = " + subListIndex);
+        System.out.println("contains = " + fullDeck.containsAll(tens));
+
+        boolean disjoint = Collections.disjoint(fullDeck, tens);
+        System.out.println("disjoint = " + disjoint);
+
+        boolean disjoint2 = Collections.disjoint(tens, kings);
+        System.out.println("disjoint2 = " + disjoint2);
+
+        separator();
+        Card tenOfHearts = Card.getNumericCard(Suit.HEART, 10);
+        fullDeck.sort(sortingAlgorithm);
+        int foundIndex = Collections.binarySearch(fullDeck, tenOfHearts, sortingAlgorithm);
+        System.out.println("foundIndex = " + foundIndex);
+        System.out.println("foundIndex = " + fullDeck.indexOf(tenOfHearts));
+        System.out.println(fullDeck.get(foundIndex));
+
+        Card tenOfClubs = Card.getNumericCard(Suit.CLUB, 10);
+        Collections.replaceAll(fullDeck, tenOfClubs, tenOfHearts);
+        Deck.printDeck(fullDeck.subList(32, 36), "Tens row");
+
+        Collections.replaceAll(fullDeck, tenOfHearts, tenOfClubs);
+        Deck.printDeck(fullDeck.subList(32, 36), "Tens row");
+
+        if (Collections.replaceAll(fullDeck, tenOfHearts, tenOfClubs)) {
+            System.out.println("Tens of hearts replaced with tens of clubs");
+        } else {
+            System.out.println("No tens of hearts found in the list");
+        }
+
+        System.out.println("Ten of Clubs Cards = " +
+                Collections.frequency(fullDeck, tenOfClubs));
+        System.out.println("Best Card = " + Collections.max(fullDeck, sortingAlgorithm));
+        System.out.println("Worst Card = " + Collections.min(fullDeck, sortingAlgorithm));
+
+        var sortBySuit = Comparator.comparing(Card::suit)
+                .thenComparing(Card::rank);
+        fullDeck.sort(sortBySuit);
+        Deck.printDeck(fullDeck, "Sorted by Suit, Rank");
+
+        List<Card> copied = new ArrayList<>(fullDeck.subList(0, 13));
+        Collections.rotate(copied, 2);
+        System.out.println("UnRotated: " + fullDeck.subList(0, 13));
+        System.out.println("Rotated " + 2 + ": " + copied);
+
+        copied = new ArrayList<>(fullDeck.subList(0, 13));
+        Collections.rotate(copied, -2);
+        System.out.println("UnRotated: " + fullDeck.subList(0, 13));
+        System.out.println("Rotated " + -2 + ": " + copied);
+
+        copied = new ArrayList<>(fullDeck.subList(0, 13));
+        for (int i = 0; i < copied.size() / 2; i++) {
+            Collections.swap(copied, i, copied.size() - 1 - i);
+        }
+        System.out.println("Manual reverse: " + copied);
+
+        copied = new ArrayList<>(fullDeck.subList(0, 13));
+        Collections.reverse(copied);
+        System.out.println("Using reverse: " + copied);
     }
 }
--- Section_15/src/com/keldorn/model/card/Deck.java
+++ Section_15/src/com/keldorn/model/card/Deck.java
@@ -1,9 +1,14 @@
 package com.keldorn.model.card;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Deck {
+    private static final String DESCRIPTION_DEFAULT = "Current Deck";
+    private static final int ROWS_DEFAULT = 4;
+    private static final int CARDS_PER_ROW = 13;
+
     private final List<Card> clubs = new CardType(Suit.CLUB).getCards();
     private final List<Card> diamonds = new CardType(Suit.DIAMOND).getCards();
     private final List<Card> hearts = new CardType(Suit.HEART).getCards();
@@ -17,10 +22,41 @@ public class Deck {
         return cards;
     }
 
+    public List<Card> getShuffledDeck() {
+        List<Card> cards = new ArrayList<>(getStandardDeck());
+        Collections.shuffle(cards);
+        return cards;
+    }
+
     public void printDeck() {
         System.out.println(clubs);
         System.out.println(diamonds);
         System.out.println(hearts);
         System.out.println(spades);
+    }
+
+    public static void printDeck(List<Card> cards) {
+        printDeck(cards, DESCRIPTION_DEFAULT);
+    }
+
+    public static void printDeck(List<Card> cards, String description) {
+        printDeck(cards, description, ROWS_DEFAULT);
+    }
+
+    public static void printDeck(List<Card> cards, String description, int rows) {
+        if (description != null && !description.isEmpty()) {
+            System.out.println("-".repeat(30));
+            System.out.println(description.trim());
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = (i * CARDS_PER_ROW); j < CARDS_PER_ROW + (i * CARDS_PER_ROW); j++) {
+                if (j >= cards.size()) {
+                    System.out.println();
+                    return;
+                }
+                System.out.print(cards.get(j) + " ");
+            }
+            System.out.println();
+        }
     }
 }
