2dfdb2701466f12ad7b50441f83121f3588f7191
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-08-05 13:29:31 +0200

    8/5/2025

--- Section_20/src/main/java/com/keldorn/Main.java
+++ Section_20/src/main/java/com/keldorn/Main.java
@@ -6,14 +6,24 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         fileExceptions();
         fileAndPath();
+        pathListings();
+        fileListings();
+        fileWalker();
     }
 
     private static void fileExceptions() {
@@ -143,6 +153,204 @@ public class Main {
             } catch (IOException e) {
                 System.out.println("Something went wrong.");
             }
+        }
+    }
+
+    private static void pathListings() {
+        Separator.separator();
+        Path path = Path.of("files/testing.txt");
+//        printPathInfo(path);
+        logStatement(path);
+        extraInfo(path);
+    }
+
+    private static void printPathInfo(Path path) {
+        System.out.println("Path: " + path);
+        System.out.println("fileName = " + path.getFileName());
+        System.out.println("parent = " + path.getParent());
+        Path absolutePath = path.toAbsolutePath();
+        System.out.println("Absolute Path = " + absolutePath);
+        System.out.println("Absolute Path Root = " + absolutePath.getRoot());
+        System.out.println("Root = " + path.getRoot());
+        System.out.println("isAbsolute = " + path.isAbsolute());
+
+        System.out.println(absolutePath.getRoot());
+//        int i = 1;
+//        var it = path.toAbsolutePath().iterator();
+//        while (it.hasNext()) {
+//            System.out.println(".".repeat(i++) + " " + it.next());
+//        }
+
+        int pathParts = absolutePath.getNameCount();
+        for (int i = 0; i < pathParts; i++) {
+            System.out.println(".".repeat(i + 1) + " " + absolutePath.getName(i));
+        }
+        Separator.separator();
+
+    }
+
+    private static void logStatement(Path path) {
+        try {
+            Path parent = path.getParent();
+            if (!Files.exists(parent)) {
+                Files.createDirectories(parent);
+            }
+            Files.writeString(path, Instant.now() +
+                    ": hello file world\n", StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void extraInfo(Path path) {
+        try {
+            var atts = Files.readAttributes(path, "*");
+            atts.entrySet().forEach(System.out::println);
+            System.out.println(Files.probeContentType(path));
+        } catch (IOException e) {
+            System.out.println("Problem getting attributes");
+        }
+    }
+
+    private static void fileListings() {
+        Separator.separator();
+        Path path = Path.of("");
+        System.out.println("cwd = " + path.toAbsolutePath());
+
+        try (Stream<Path> paths = Files.list(path)) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Separator.separator();
+        try (Stream<Path> paths = Files.walk(path, 2)) {
+            paths
+//                    .filter(Files::isRegularFile)
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Separator.separator();
+        try (Stream<Path> paths = Files.find(path, Integer.MAX_VALUE,
+                (p, attr) -> attr.isRegularFile() && attr.size() > 300
+        )) {
+            paths
+                    .map(Main::listDir)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        path = path.resolve(".idea");
+        Separator.separatorWithHeader("Directory Stream");
+        try (var dirs = Files.newDirectoryStream(path, "*.xml")) {
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Separator.separatorWithHeader("Directory Stream");
+        try (var dirs = Files.newDirectoryStream(path,
+                p -> p.getFileName().toString().endsWith(".xml")
+                && Files.isRegularFile(p) && Files.size(p) > 1000
+                )) {
+            dirs.forEach(d -> System.out.println(Main.listDir(d)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String listDir(Path path) {
+        try {
+            boolean isDir = Files.isDirectory(path);
+            FileTime dateField = Files.getLastModifiedTime(path);
+            LocalDateTime modDT = LocalDateTime.ofInstant(
+                    dateField.toInstant(), ZoneId.systemDefault()
+            );
+            return "%tD %tT %-5s %12s %s"
+                    .formatted(modDT, modDT, (isDir ? "<DIR>" : ""),
+                            (isDir ? "" : Files.size(path)), path);
+        } catch (IOException e) {
+            System.out.println("Whoops! Something went wrong with " + path);
+            return path.toString();
+        }
+    }
+
+    private static void fileWalker() {
+        Separator.separator();
+        Path startingPath = Path.of("..");
+        FileVisitor<Path> statsVisitor = new StatsVisitor(1);
+        try {
+            Files.walkFileTree(startingPath, statsVisitor);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static class StatsVisitor extends SimpleFileVisitor<Path> {
+        private Path initialPath = null;
+        private final Map<Path, Long> folderSizes = new LinkedHashMap<>();
+        private int initialCount;
+        private int printLevel;
+
+        public StatsVisitor(int printLevel) {
+            this.printLevel = printLevel;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(file);
+            Objects.requireNonNull(attrs);
+            folderSizes.merge(file.getParent(), 0L, (o, m) -> o += attrs.size());
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            Objects.requireNonNull(dir);
+            Objects.requireNonNull(attrs);
+            if (initialPath == null) {
+                initialPath = dir;
+                initialCount = dir.getNameCount();
+            } else {
+                int relativeLevel = dir.getNameCount() - initialCount;
+                if (relativeLevel == 1) {
+                    folderSizes.clear();
+                }
+                folderSizes.put(dir, 0L);
+            }
+            return FileVisitResult.CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            Objects.requireNonNull(dir);
+//            if (exc != null)
+//                throw exc;
+            if (dir.equals(initialPath)) {
+                return FileVisitResult.TERMINATE;
+            }
+
+            int relativeLevel = dir.getNameCount() - initialCount;
+            if (relativeLevel == 1) {
+                folderSizes.forEach((key, value) -> {
+                    int level = key.getNameCount() - initialCount - 1;
+                    if (level < printLevel) {
+                        System.out.printf("%s[%s] - %,d bytes %n",
+                                "\t".repeat(level), key.getFileName(), value);
+                    }
+                });
+            } else {
+                long folderSize = folderSizes.get(dir);
+                folderSizes.merge(dir.getParent(), 0L, (o, n) -> o += folderSize);
+            }
+            return FileVisitResult.CONTINUE;
         }
     }
 }
--- Section_20/src/main/java/com/keldorn/util/Separator.java
+++ Section_20/src/main/java/com/keldorn/util/Separator.java
@@ -17,4 +17,17 @@ public final class Separator {
     public static void separator(Character character, int length) {
         System.out.println(character.toString().repeat(length));
     }
+
+    public static void separatorWithHeader(String header) {
+        separatorWithHeader('=', header);
+    }
+
+    public static void separatorWithHeader(Character character, String header) {
+        separatorWithHeader(character, 10, header);
+    }
+
+    public static void separatorWithHeader(Character character, int length, String header) {
+        String separator = character.toString().repeat(length);
+        System.out.printf("%s %s %s%n", separator, header, separator);
+    }
 }
