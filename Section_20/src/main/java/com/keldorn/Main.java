e986233485363563b98c9507541c752575896de4
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-08-08 21:30:29 +0200

    8/8/2025

--- Section_20/src/main/java/com/keldorn/Main.java
+++ Section_20/src/main/java/com/keldorn/Main.java
@@ -22,7 +22,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        dirChallenge();
+
     }
 
     private static void fileExceptions() {
@@ -841,5 +841,49 @@ public class Main {
         }
     }
 
+    private static final Map<Long, Long> indexedIds = new LinkedHashMap<>();
+    private static int recordsInFile = 0;
 
+    static {
+        try (RandomAccessFile rb = new RandomAccessFile("student.idx", "r")) {
+            loadIndex(rb, 0);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void randomAccess() {
+        Separator.separatorWithHeader("Random Access");
+//        BuildStudentData.build("student", true);
+        try (RandomAccessFile ra = new RandomAccessFile("student.dat", "r")) {
+//            loadIndex(ra, 0);
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter a Student Id or 0 to quit");
+            while (scanner.hasNext()) {
+                long studentId = Long.parseLong(scanner.nextLine());
+                if (studentId < 1) {
+                    break;
+                }
+                ra.seek(indexedIds.get(studentId));
+                String targetedRecord = ra.readUTF();
+                System.out.println(targetedRecord);
+                System.out.println("Enter another Student Id or 0 to quit");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void loadIndex(RandomAccessFile ra, int indexPosition) {
+        try {
+            ra.seek(indexPosition);
+            recordsInFile = ra.readInt();
+            System.out.println(recordsInFile);
+            for (int i = 0; i < recordsInFile; i++) {
+                indexedIds.put(ra.readLong(), ra.readLong());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
