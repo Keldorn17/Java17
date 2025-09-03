8d5953401719bbb3cc5746b2fe2cea75262b1d23
Patai Zoltán <patai17.zoltan17@gmail.com>
2025-09-03 15:47:15 +0200

    9/3/2025

---
+++ Section_23/src/main/java/com/keldorn/server/SimpleServer.java
@@ -0,0 +1,31 @@
+package main.java.com.keldorn.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(5000)) {
+            try (Socket socket = serverSocket.accept()) {
+                System.out.println("Sever accepts client connection");
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+                while (true) {
+                    String echoString = input.readLine();
+                    System.out.println("Sever got request data: " + echoString);
+                    if (echoString.equals("exit")) {
+                        break;
+                    }
+                    output.println("Echo from server: " + echoString);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Sever exception " + e.getMessage());
+        }
+    }
+}
